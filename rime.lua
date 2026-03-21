b598885cd8345acf6ee46c7453020250339cfa2a
Sha1rholder <sha1rholder@outlook.com>
2026-03-22 02:08:40 +0800

    Save current Rime config before simplification filtering changes

---
+++ rime.lua
@@ -0,0 +1,3 @@
+charset_comment_filter = require("charset_comment_filter") --Unicode分区提示
+core2022 = require("core2022_filter") --自定义字符集过滤（常用字集）
+lua_unicode_display_filter = require("unicode_display")  --Unicode编码提示
