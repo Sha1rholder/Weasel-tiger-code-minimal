7f3982d5593b6ef2ac3947c1042dd015e98c0ff8
Sha1rholder <sha1rholder@outlook.com>
2026-03-22 02:22:51 +0800

    Enforce simplified-only tigress candidates with whitelist filter

--- rime.lua
+++ rime.lua
@@ -1,3 +1,4 @@
 charset_comment_filter = require("charset_comment_filter") --Unicode分区提示
 core2022 = require("core2022_filter") --自定义字符集过滤（常用字集）
+tigress_simp_filter = require("tigress_simp_filter") --虎码简体白名单过滤
 lua_unicode_display_filter = require("unicode_display")  --Unicode编码提示
